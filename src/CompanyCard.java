e1d82d591705a23f0d24af26d40772f20ae581df
Hugant <HugantMD@yandex.ru>
2017-10-14 23:13:43 +0700

    Modified method isValid()

--- src/CompanyCard.java
+++ src/CompanyCard.java
@@ -17,6 +17,13 @@ public class CompanyCard {
 	}
 	
 	public boolean isValid() {
+		if (properties.get(Main.HEADER.get("CompanyName")).equals("")) 	return false;
+		if (properties.get(Main.HEADER.get("Website")).equals("")) 		return false;
+		if (properties.get(Main.HEADER.get("Address")).equals("")) 		return false;
+		if (properties.get(Main.HEADER.get("Sphere")).equals("")) 		return false;
+		if (properties.get(Main.HEADER.get("Phone")).equals("")) 		return false;
+		if (properties.get(Main.HEADER.get("City")).equals("")) 		return false;
+		
 		return true;
 	}
 }
